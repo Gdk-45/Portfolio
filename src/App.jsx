72207230a5a50db5f8c288f9ac4f7e6fe8f4db63
Ajay Dattu <dattuajay005@gmail.com>
2024-12-01 23:38:20 +0530

    #addded custom ursor

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import './App.css';
 import Home from './Pages/MainPage';
 import QuoteAnimation from './Pages/WordPullup';
 import About from './Pages/About';
+import MouseFollower from './components/Customcursor';
 
 function App() {
   return (
     <Router className="[background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+      <MouseFollower/>
       <Routes>
         <Route path="/" element={<QuoteAnimation/>} />
         <Route path="/MainPage" element={<Home />} />
